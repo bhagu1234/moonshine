118a46d4f941fbcba7ab4a2932535693f0be32fb
bhagu1234 <team@veravalonline.com>
2023-07-14 09:53:51 +0530

    #Backup

---
+++ app/Http/Controllers/CustomAuthController.php
@@ -0,0 +1,81 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use Hash;
+use Session;
+use App\Models\subscription_admin;
+use Illuminate\Support\Facades\Auth;
+class CustomAuthController extends Controller
+{
+    public function index()
+    {
+        return view('auth.login');
+    }  
+      
+    public function customLogin(Request $request)
+    {
+        // dd('hh');
+        // dd($request);
+        // $request->validate([
+        //     'admin_email' => 'required',
+        //     'admin_password' => 'required',
+        // ]);
+  
+        $credentials = $request->only('admin_email', 'admin_password');
+        // $credentials['admin_password'] = sha1($credentials['admin_password']);
+        // dd($credentials);
+        if (Auth::attempt($credentials)) {
+            return redirect()->intended('dashboard')
+                        ->withSuccess('Signed in');
+        }
+  
+        return redirect("login")->withSuccess('Login details are not valid');
+    }
+
+    public function registration()
+    {
+        return view('auth.registration');
+    }
+      
+    public function customRegistration(Request $request)
+    {  
+        // dd($request);
+        // $request->validate([
+        //     'company_name' => 'required',
+        //     'email' => 'required|email|unique:users',
+        //     'password' => 'required|min:6',
+        // ]);
+           
+        $data = $request->all();
+        $check = $this->create($data);
+         
+        return redirect("dashboard")->withSuccess('You have signed-in');
+    }
+
+    public function create(array $data)
+    {
+        // dd($data);
+      return subscription_admin::create([
+        'company_name' => $data['company_name'],
+        'admin_email' => $data['admin_email'],
+        'admin_password' => Hash::make($data['admin_password'])
+      ]);
+    }    
+    
+    public function dashboard()
+    {
+       
+        if(Auth::check()){
+            return view('dashboard');
+        }
+  
+        return redirect("login")->withSuccess('You are not allowed to access');
+    }
+    
+    public function signOut() {
+        Session::flush();
+        Auth::logout();
+  
+        return Redirect('login');
+    }
+}
---
+++ app/Models/subscription_admin.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class subscription_admin extends Model implements Authenticatable
+{
+    use HasFactory;
+
+    protected $table = 'subscription_admin';
+    protected $primaryKey = 'admin_id';
+
+    protected $fillable = [
+        'company_name',
+        'admin_email',
+        'admin_password',
+    ];
+
+    /**
+     * Get the name of the unique identifier for the user.
+     *
+     * @return string
+     */
+    public function getAuthIdentifierName()
+    {
+        return 'admin_id'; // Replace 'admin_id' with the name of the unique identifier column in your 'subscription_admin' table
+    }
+
+    /**
+     * Get the unique identifier for the user.
+     *
+     * @return mixed
+     */
+    public function getAuthIdentifier()
+    {
+        return $this->{$this->getAuthIdentifierName()};
+    }
+
+    /**
+     * Get the password for the user.
+     *
+     * @return string
+     */
+    public function getAuthPassword()
+    {
+        return $this->admin_password;
+    }
+
+    /**
+     * Get the remember token for the user.
+     *
+     * @return string|null
+     */
+    public function getRememberToken()
+    {
+        return $this->remember_token;
+    }
+
+    /**
+     * Set the remember token for the user.
+     *
+     * @param  string|null  $value
+     * @return void
+     */
+    public function setRememberToken($value)
+    {
+        $this->remember_token = $value;
+    }
+
+    /**
+     * Get the name of the remember token column.
+     *
+     * @return string
+     */
+    public function getRememberTokenName()
+    {
+        return 'remember_token';
+    }
+}
